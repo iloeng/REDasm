b7445030fcc6b503b072ef3118df7e0f92447e0e
Dax <contact@antoniodavide.dev>
2026-06-07 18:23:50 +0200

    chore: refactoring

--- cmake/Compiler.cmake
+++ cmake/Compiler.cmake
@@ -1,69 +1,69 @@
-function(setup_compiler TARGET)
-set_target_properties(${TARGET}
-    PROPERTIES
+function(setup_compiler project_name)
+    set_target_properties(${project_name}
+        PROPERTIES
         EXPORT_COMPILE_COMMANDS ON
         CXX_STANDARD_REQUIRED YES
         CXX_EXTENSIONS NO
         CXX_STANDARD 17
-)
+    )
 
-target_compile_features(${TARGET}
-    PRIVATE
+    target_compile_features(${project_name}
+        PRIVATE
         cxx_std_17
-)
-
-set(NOT_MSVC_COMPILE_OPTIONS
-    "-Wall"
-    "-Wextra"
-    "-Wpedantic"
-    "-Wno-error=unused"
-    "-Wno-error=unused-function"
-    "-Wno-error=unused-parameter"
-    "-Wno-error=unused-value"
-    "-Wno-error=unused-variable"
-    "-Wno-error=unused-local-typedefs"
-    "-Wno-error=unused-but-set-parameter"
-    "-Wno-error=unused-but-set-variable"
-    "-fno-rtti"
-)
-
-set(NOT_MSVC_LINK_OPTIONS)
-
-set(MSVC_COMPILE_OPTIONS
-    "/W4"
-)
-
-set(MSVC_LINK_OPTIONS)
-
-if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-    list(APPEND NOT_MSVC_COMPILE_OPTIONS 
-        "-Werror"
-        "-g"
-        "-fsanitize=address,undefined"
-        "-fno-omit-frame-pointer"
     )
 
-    list(APPEND NOT_MSVC_LINK_OPTIONS
-        "-fsanitize=address,undefined"
-        "-fno-omit-frame-pointer"
+    set(NOT_MSVC_COMPILE_OPTIONS
+        "-Wall"
+        "-Wextra"
+        "-Wpedantic"
+        "-Wno-error=unused"
+        "-Wno-error=unused-function"
+        "-Wno-error=unused-parameter"
+        "-Wno-error=unused-value"
+        "-Wno-error=unused-variable"
+        "-Wno-error=unused-local-typedefs"
+        "-Wno-error=unused-but-set-parameter"
+        "-Wno-error=unused-but-set-variable"
+        "-fno-rtti"
     )
 
-    list(APPEND MSVC_COMPILE_OPTIONS
-        "/WX"
-        "/Od"
-        # "/fsanitize=address"
-    )
-endif()
+    set(NOT_MSVC_LINK_OPTIONS)
 
-target_compile_options(${TARGET}
-    PRIVATE
+    set(MSVC_COMPILE_OPTIONS
+        "/W4"
+    )
+
+    set(MSVC_LINK_OPTIONS)
+
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+        list(APPEND NOT_MSVC_COMPILE_OPTIONS 
+            "-Werror"
+            "-g"
+            "-fsanitize=address,undefined"
+            "-fno-omit-frame-pointer"
+        )
+
+        list(APPEND NOT_MSVC_LINK_OPTIONS
+            "-fsanitize=address,undefined"
+            "-fno-omit-frame-pointer"
+        )
+
+        list(APPEND MSVC_COMPILE_OPTIONS
+            "/WX"
+            "/Od"
+            # "/fsanitize=address"
+        )
+    endif()
+
+    target_compile_options(${project_name}
+        PRIVATE
         $<$<CXX_COMPILER_ID:MSVC>:${MSVC_COMPILE_OPTIONS}>
         $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:${NOT_MSVC_COMPILE_OPTIONS}>
-)
+    )
 
-target_link_options(${TARGET}
-    PUBLIC
+    target_link_options(${project_name}
+        PUBLIC
         $<$<CXX_COMPILER_ID:MSVC>:${MSVC_LINK_OPTIONS}>
         $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:${NOT_MSVC_LINK_OPTIONS}>
-)
+    )
 endfunction()
