805b9faab2840f063533a246d79a7f7c71d8838c
Dax <contact@antoniodavide.dev>
2026-06-08 15:14:07 +0200

    fix: don't hardcode /usr/lib and use REDASM_PLUGIN_DIR for appimage

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ void configure_searchpaths() {
 
         // 2. bundled plugins
         const QString APP_DIR = QString::fromUtf8(appimage_dir);
-        append_unique(utils::search_paths, APP_DIR + "/usr/lib/redasm/plugins");
+        append_unique(utils::search_paths, QString::fromUtf8(REDASM_PLUGIN_DIR));
         append_unique(utils::kb_search_paths, (APP_DIR + "/usr/share/redasm/kb").toUtf8());
 
         // 3. system plugins
