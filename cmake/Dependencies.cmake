ae3af817211e3bc3c4b739de953ad0f45c8d5b25
Dax <contact@antoniodavide.dev>
2026-07-21 11:19:45 +0200

    chore: bump QHexView

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -6,7 +6,7 @@ qt_standard_project_setup()
 function(setup_dependencies)
     CPMAddPackage(
         NAME QHexView
-        VERSION "5.1.2"
+        VERSION "5.1.3"
         # GIT_TAG "master"
         GITHUB_REPOSITORY "Dax89/QHexView"
 
